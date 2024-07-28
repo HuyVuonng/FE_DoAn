b95a6ab519c9d63598b204bac010fedb61e95d31
HuyVuong <vuonghuy12345@gmail.com>
2024-07-28 23:10:49 +0700

    style suggest item

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  // API_URL: 'https://hostelbanking.somee.com/api',
-  API_URL: 'https://localhost:7204/api',
+  API_URL: 'https://hostelbanking.somee.com/api',
+  // API_URL: 'https://localhost:7204/api',
 
   name: 'dev',
   firebaseConfig: {
